df7847e31a260953fd792821251cf238c4abc07a
rowanweathers <74547960+rowanweathers@users.noreply.github.com>
2020-12-05 14:24:45 -0800

    Adding integration test for Book() + isSource()

---
+++ test/Book.isSource.integration.test.js
@@ -0,0 +1,16 @@
+import chai from 'chai'
+import mocha from 'mocha'
+import { Book } from '../Book.js'
+import { isSource } from '../isSource.js'
+
+var describe = mocha.describe
+var expect = chai.expect
+var it = mocha.it
+
+const emptyBook = new Book()
+
+describe('new Book + isSource()', () => {
+  it('An instance of Book() is a source.', function () {
+    expect(isSource(new Book())).to.be.true
+  })
+})
