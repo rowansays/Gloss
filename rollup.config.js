080d772505812fb82e92f2d1ccddbf51d15e940d
rowanweathers <74547960+rowanweathers@users.noreply.github.com>
2020-12-05 13:42:35 -0800

    Add Rollup configuration.

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import commonjs from '@rollup/plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
+export default [
+  {
+    input: 'index.js',
+    output: {
+      file: 'dist/symbolism.iife.js',
+      format: 'iife',
+      name: 'Symbolism'
+    },
+    plugins: [
+      commonjs(),
+      nodeResolve(),
+      terser({
+        mangle: {
+          reserved: ['Book']
+        }
+      })
+    ]
+  }
+]
