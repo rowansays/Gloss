9a6daff1a69710adf94f31c5879033c704104997
rowanweathers <74547960+rowanweathers@users.noreply.github.com>
2020-12-05 16:42:38 -0800

    Add Webpage constructor with unit tests.

---
+++ Webpage.js
@@ -0,0 +1,47 @@
+import { freeze } from '@mfields/lib/.internal/freeze.js'
+import { makeInstanceOf } from '@mfields/lib/makeInstanceOf.js'
+
+/**
+ * A Webpage is a type of Source.
+ */
+function Webpage (key, title, author, url) {
+  if (!(this instanceof Webpage)) {
+    return makeInstanceOf(Webpage, arguments)
+  }
+
+  this.author = author && typeof author === 'string' ? author : ''
+  this.key = key && typeof key === 'string' ? key : ''
+  this.title = title && typeof title === 'string' ? title : ''
+  this.url = url && typeof url === 'string' ? url : ''
+
+  freeze(this, Webpage)
+}
+Webpage.prototype.getType = function () {
+  return 'Webpage'
+}
+Webpage.prototype.getName = function () {
+  return this.title
+}
+Webpage.prototype.getUrl = function () {
+  return this.url
+}
+Webpage.fromObject = function (o) {
+  return new Webpage(
+    o && o.key ? o.key : '',
+    o && o.title ? o.title : '',
+    o && o.author ? o.author : '',
+    o && o.url ? o.url : ''
+  )
+}
+
+function cleanDateString (dirty) {
+  if (typeof dirty === 'string') {
+    return dirty
+  }
+  if (typeof dirty === 'number') {
+    return String(dirty)
+  }
+  return ''
+}
+
+export { Webpage }
---
+++ test/Webpage.unit.test.js
@@ -0,0 +1,142 @@
+import chai from 'chai'
+import mocha from 'mocha'
+import { Webpage } from '../Webpage.js'
+
+var describe = mocha.describe
+var expect = chai.expect
+var it = mocha.it
+
+const emptyWebpage = new Webpage()
+const wikipediaWebpage = new Webpage(
+  'wikipedia:unitTesting',
+  'Unit Testing',
+  'The contributors',
+  'https://en.wikipedia.org/wiki/Unit_testing'
+)
+
+describe('Webpage()', () => {
+  it('is a function.', () => {
+    expect(typeof Webpage).to.equal('function')
+  })
+  it('can be constructed without the "new" keyword.', function () {
+    expect(function () { Webpage() }).not.to.throw(Error)
+  })
+  it('creates frozen instances.', function () {
+    expect(Object.isFrozen(Webpage())).to.equal(true)
+    if (typeof Webpage().__proto__ === 'object') {
+      expect(Object.isFrozen(Webpage().__proto__)).to.equal(true)
+    }
+  })
+  it('does not accidentally freeze the built-in Object prototype.', function () {
+    expect(Object.isFrozen(Object.prototype)).to.equal(false)
+  })
+})
+describe('Webpage(): Parameters', function () {
+  describe('1. key', () => {
+    it('accepts a string as parameter one.', () => {
+      expect(function () { new Webpage('') }).not.to.throw(Error)
+    })
+  })
+  describe('2. title', () => {
+    it('accepts a string as parameter two.', () => {
+      expect(function () { new Webpage('', '') }).not.to.throw(Error)
+    })
+  })
+  describe('3. author', () => {
+    it('accepts a string as parameter 3.', () => {
+      expect(function () { new Webpage('', '', '') }).not.to.throw(Error)
+    })
+  })
+  describe('4. url', () => {
+    it('accepts a string as parameter 4.', () => {
+      expect(function () { new Webpage('', '', '', '') }).not.to.throw(Error)
+    })
+  })
+})
+describe('Webpage: Instance Properties', function () {
+  describe('author', function () {
+    it('defaults to an empty string.', () => {
+      expect(emptyWebpage.author).to.equal('')
+    })
+    it('inherits the value of parameter two.', () => {
+      expect(wikipediaWebpage.author).to.equal('The contributors')
+    })
+  })
+  describe('key', function () {
+    it('defaults to an empty string.', () => {
+      expect(emptyWebpage.key).to.equal('')
+    })
+    it('inherits the value of parameter two.', () => {
+      expect(wikipediaWebpage.key).to.equal('wikipedia:unitTesting')
+    })
+  })
+  describe('title', function () {
+    it('defaults to an empty string.', () => {
+      expect(emptyWebpage.title).to.equal('')
+    })
+    it('inherits the value of parameter two.', () => {
+      expect(wikipediaWebpage.title).to.equal('Unit Testing')
+    })
+  })
+  describe('url', function () {
+    it('defaults to an empty string.', () => {
+      expect(emptyWebpage.url).to.equal('')
+    })
+    it('inherits the value of parameter two.', () => {
+      expect(wikipediaWebpage.url).to.equal('https://en.wikipedia.org/wiki/Unit_testing')
+    })
+  })
+})
+describe('Webpage: Instance Methods', function () {
+  describe('getName()', function () {
+    it('is a function.', () => {
+      expect(typeof emptyWebpage.getName).to.equal('function')
+    })
+    it('returns value of title property.', () => {
+      expect(wikipediaWebpage.getName()).to.equal('Unit Testing')
+    })
+  })
+  describe('getType()', function () {
+    it('is a function.', () => {
+      expect(typeof emptyWebpage.getType).to.equal('function')
+    })
+    it('always returns "Webpage".', () => {
+      expect(emptyWebpage.getType()).to.equal('Webpage')
+      expect(wikipediaWebpage.getType()).to.equal('Webpage')
+    })
+  })
+  describe('getUrl()', function () {
+    it('is a function.', () => {
+      expect(typeof emptyWebpage.getUrl).to.equal('function')
+    })
+    it('returns the value of the "url" property.', () => {
+      const url = wikipediaWebpage.getUrl()
+      expect(url).to.equal('https://en.wikipedia.org/wiki/Unit_testing')
+    })
+  })
+})
+describe('Webpage.fromObject()', function () {
+  it('is a function.', () => {
+    expect(typeof Webpage.fromObject).to.equal('function')
+  })
+  it('constructs an empty book by default.', () => {
+    const webpage = Webpage.fromObject()
+    expect(webpage.key).to.equal('')
+    expect(webpage.title).to.equal('')
+    expect(webpage.author).to.equal('')
+    expect(webpage.url).to.equal('')
+  })
+  it('constructs an valid book from an object.', () => {
+    const obj = {
+      url: 'https://en.wikipedia.org/wiki/Unit_testing',
+      title: 'Unit Testing',
+      key: 'wikipedia:unitTesting',
+      author: 'The contributors',
+    }
+    const webpage = Webpage.fromObject(obj)
+    expect(webpage.key).to.equal(obj.key)
+    expect(webpage.title).to.equal(obj.title)
+    expect(webpage.author).to.equal(obj.author)
+    expect(webpage.url).to.equal(obj.url)
+  })
+})
