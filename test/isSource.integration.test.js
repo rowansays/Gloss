7af0c042f979345573bcbd9bfeb29c222ff9e34d
rowanweathers <74547960+rowanweathers@users.noreply.github.com>
2020-12-05 17:38:19 -0800

    Add Webpage() to isSource() integration tests.

--- test/isSource.integration.test.js
+++ test/isSource.integration.test.js
@@ -1,16 +1,19 @@
 import chai from 'chai'
 import mocha from 'mocha'
-import { Book } from '../Book.js'
 import { isSource } from '../isSource.js'
+import { Book } from '../Book.js'
+import { Webpage } from '../Webpage.js'
 
 var describe = mocha.describe
 var expect = chai.expect
 var it = mocha.it
 
-const emptyBook = new Book()
-
-describe('isSource(Book())', () => {
+describe('isSource() + various constructors.', () => {
+  const emptyBook = new Book()
   it('An instance of Book() is a source.', function () {
     expect(isSource(new Book())).to.be.true
+  })
+  it('Instances of Webpage() are sources.', function () {
+    expect(isSource(Webpage())).to.be.true
   })
 })
