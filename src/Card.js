be9a06c789446205b0f7bf1d30f1e1e0fb6d2139
rowanweathers <74547960+rowanweathers@users.noreply.github.com>
2020-12-06 20:59:07 -0800

    Card(): Remove unneeded getUrl() method.

--- src/Card.js
+++ src/Card.js
@@ -2,17 +2,14 @@ import { freeze } from '@mfields/lib/.internal/freeze.js'
 import { AbstractWork } from '../Abstracts/AbstractWork.js'
 
 /**
- * A Card is a concrete representation of an abstract work.
+ * A Card is a concrete representation of an abstract work
+ * with no extra parameters.
  */
 function Card () {
   AbstractWork.call(this, ...arguments)
 }
 
 Card.prototype = Object.create(AbstractWork.prototype)
-
-Card.prototype.getUrl = function () {
-  return this.url
-}
 
 function Factory () {
   const card = new Card(...arguments)
