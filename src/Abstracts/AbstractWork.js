befad8e4a4b71f6e17341e0ae8eebc862ffbcc0f
rowanweathers <74547960+rowanweathers@users.noreply.github.com>
2020-12-06 20:54:46 -0800

    Refactor AbstractWork

--- src/Abstracts/AbstractWork.js
+++ src/Abstracts/AbstractWork.js
@@ -1,8 +1,31 @@
-function AbstractWork (key, title, subtitle, description) {
-  this.key = key && typeof key === 'string' ? key.trim() : ''
-  this.subtitle = subtitle && typeof subtitle === 'string' ? subtitle.trim() : ''
-  this.title = title && typeof title === 'string' ? title.trim() : ''
-  this.description = description && typeof description === 'string' ? description.trim() : ''
+import { isWork } from '../Predicates/isWork.js'
+
+function AbstractWork (key, title, subtitle, description, author, date, url) {
+  console.log(typeof arguments[0])
+  if (arguments.length === 1 && isWork(arguments[0])) {
+    const work = arguments[0]
+    this.author = work.getAuthor()
+    this.date = work.getDate()
+    this.description = work.getDescription()
+    this.key = work.getKey()
+    this.subtitle = work.getSubtitle()
+    this.title = work.getTitle()
+    this.url = work.getUrl()
+  } else {
+    this.author = author && typeof author === 'string' ? author.trim() : ''
+    this.date = cleanDateString(date)
+    this.description = description && typeof description === 'string' ? description.trim() : ''
+    this.key = key && typeof key === 'string' ? key.trim() : ''
+    this.subtitle = subtitle && typeof subtitle === 'string' ? subtitle.trim() : ''
+    this.title = title && typeof title === 'string' ? title.trim() : ''
+    this.url = url && typeof url === 'string' ? url.trim() : ''
+  }
+}
+AbstractWork.prototype.getAuthor = function () {
+  return this.author
+}
+AbstractWork.prototype.getDate = function () {
+  return this.date
 }
 AbstractWork.prototype.getDescription = function () {
   return this.description
@@ -21,6 +44,17 @@ AbstractWork.prototype.getSubtitle = function () {
 }
 AbstractWork.prototype.getTitle = function () {
   return this.title
+}
+AbstractWork.prototype.getUrl = function () {
+  return this.url
+}
+
+function cleanDateString (dirty) {
+  switch (typeof dirty) {
+    case 'string': return dirty
+    case 'number': return String(dirty)
+    default: return ''
+  }
 }
 
 export { AbstractWork }
---
+++ src/Predicates/isWork.js
@@ -0,0 +1,19 @@
+/**
+ * Can the provided value be used as a work?
+ *
+ * @param {mixed} aught The value which may be a work.
+ * @return {bool} True if the object can be used as a work; false otherwise.
+ */
+function isWork (aught) {
+  return !!aught &&
+    typeof aught === 'object' &&
+    typeof aught.getAuthor === 'function' &&
+    typeof aught.getDate === 'function' &&
+    typeof aught.getDescription === 'function' &&
+    typeof aught.getKey === 'function' &&
+    typeof aught.getSubtitle === 'function' &&
+    typeof aught.getTitle === 'function' &&
+    typeof aught.getUrl === 'function'
+}
+
+export { isWork }
