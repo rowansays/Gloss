a53332421e7d00caf0aebee50611cbe5495e7a9b
rowanweathers <74547960+rowanweathers@users.noreply.github.com>
2020-12-07 19:19:08 -0800

    index.php: Add comments.

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-export { AbstractWork } from './src/Abstracts/AbstractWork.js'
-export { Book } from './src/Sources/Book.js'
-export { Card } from './src/Card.js'
-export { isCitation } from './src/Predicates/isCitation.js'
-export { isSource } from './src/Predicates/isSource.js'
-export { isWork } from './src/Predicates/isWork.js'
-export { Phrase } from './src/Citations/Phrase.js'
-export { Pos } from './src/Citations/Pos.js'
-export { Term } from './src/Entries/Term.js'
-export { Webpage } from './src/Sources/Webpage.js'
+
+// Abstracts
+  export { AbstractWork } from './src/Abstracts/AbstractWork.js'
+
+// Entries
+  export { Term } from './src/Entries/Term.js'
+
+// Predicate
+  export { isCitation } from './src/Predicates/isCitation.js'
+  export { isSource } from './src/Predicates/isSource.js'
+  export { isWork } from './src/Predicates/isWork.js'
+
+// Quotes
+  export { Phrase } from './src/Citations/Phrase.js'
+  export { Pos } from './src/Citations/Pos.js'
+
+// References
+  export { Book } from './src/Sources/Book.js'
+  export { Webpage } from './src/Sources/Webpage.js'
+
+// Everything Else
+  export { Card } from './src/Card.js'
