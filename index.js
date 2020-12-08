341069331d76fcc40ac01c5049c361b32ff2ec97
rowanweathers <74547960+rowanweathers@users.noreply.github.com>
2020-12-07 23:57:19 -0800

    index.js: remove whitespace.

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-
 // Abstracts
-  export { AbstractWork } from './src/Abstracts/AbstractWork.js'
+export { AbstractWork } from './src/Abstracts/AbstractWork.js'
 
 // Entries
-  export { Term } from './src/Entries/Term.js'
+export { Term } from './src/Entries/Term.js'
+
+// Glossaries
+export { Glossary } from './src/Glossaries/Glossary.js'
 
 // Predicate
-  export { isCitation } from './src/Predicates/isCitation.js'
-  export { isSource } from './src/Predicates/isSource.js'
-  export { isWork } from './src/Predicates/isWork.js'
+export { isCitation } from './src/Predicates/isCitation.js'
+export { isSource } from './src/Predicates/isSource.js'
+export { isWork } from './src/Predicates/isWork.js'
 
 // Quotes
-  export { Phrase } from './src/Citations/Phrase.js'
-  export { Pos } from './src/Citations/Pos.js'
+export { Phrase } from './src/Citations/Phrase.js'
+export { Pos } from './src/Citations/Pos.js'
 
 // References
-  export { Book } from './src/Sources/Book.js'
-  export { Webpage } from './src/Sources/Webpage.js'
+export { Book } from './src/Sources/Book.js'
+export { Webpage } from './src/Sources/Webpage.js'
 
 // Everything Else
-  export { Card } from './src/Card.js'
+export { Card } from './src/Card.js'
