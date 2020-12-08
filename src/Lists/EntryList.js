e7bfd77181050b4b92a319667905f071798bbb80
rowanweathers <74547960+rowanweathers@users.noreply.github.com>
2020-12-08 00:05:28 -0800

    Do not export 'Factory'. Instead use a 'doppelgänger closure'.

--- src/Lists/EntryList.js
+++ src/Lists/EntryList.js
@@ -3,28 +3,25 @@ import { AbstractObjectList } from '../Abstracts/AbstractObjectList.js'
 import { isEntry } from '../Predicates/isEntry.js'
 
 function EntryList (...entries) {
-  AbstractObjectList.call(this, ...arguments)
-  this.items = []
-  entries.forEach(entry => {
-    if (isEntry(entry)) {
-      this.items.push(entry)
-    } else {
-      console.log('NOT isEntry()', entry)
-    }
+  function EntryList (entries) {
+    AbstractObjectList.call(this)
+    this.items = []
+    entries.forEach(entry => {
+      if (isEntry(entry)) {
+        this.items.push(entry)
+      }
+    })
+  }
+
+  EntryList.prototype = Object.create(AbstractObjectList.prototype)
+
+  Object.defineProperty(EntryList.prototype, 'constructor', {
+    value: EntryList
   })
-}
 
-EntryList.prototype = Object.create(AbstractObjectList.prototype)
-Object.defineProperty(EntryList.prototype, 'constructor', {
-  value: EntryList,
-  enumerable: false,
-  writable: false
-})
-
-function Factory () {
-  const list = new EntryList(...arguments)
+  const list = new EntryList(...entries)
   freeze(list, EntryList)
   return list
 }
 
-export { Factory as EntryList }
+export { EntryList }
