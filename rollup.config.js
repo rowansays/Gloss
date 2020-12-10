677a0d844737267f1a8c6b8fdd8e68251944f8a0
rowanweathers <74547960+rowanweathers@users.noreply.github.com>
2020-12-10 14:33:54 -0800

    Rollup: remove terser plugin and config.

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import commonjs from '@rollup/plugin-commonjs'
-import { terser } from 'rollup-plugin-terser'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 
 export default [
@@ -12,53 +11,7 @@ export default [
     },
     plugins: [
       commonjs(),
-      nodeResolve(),
-      terser({
-        mangle: {
-          reserved: [
-            // Abstracts
-            'AbstractObjectList',
-            'AbstractQuote',
-            'AbstractScalarList',
-            'AbstractWork',
-
-            // Glossaries
-            'Glossary',
-            'HybridGlossary',
-
-            // Glosses
-            'Term',
-
-            // Lists
-            'GlossList',
-            'QuoteList',
-            'ReferenceList',
-            'StringList',
-
-            // Predicates
-            'isGloss',
-            'isList',
-            'isQuote',
-            'isReference',
-            'isWork',
-
-            // Quotes
-            'Normal',
-            'Phrase',
-            'Speech',
-
-            // References
-            'Book',
-            'Webpage',
-
-            // Utility
-            'castString',
-
-            // Other
-            'Card'
-          ]
-        }
-      })
+      nodeResolve()
     ]
   }
 ]
