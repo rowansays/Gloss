412dbcfed3591ec36182c6de775da986a758ea66
rowanweathers <74547960+rowanweathers@users.noreply.github.com>
2020-12-10 14:07:19 -0800

    Remove debug code.

--- test/Term.integration.test.js
+++ test/Term.integration.test.js
@@ -13,7 +13,6 @@ describe('Term(): Integration Tests', function () {
       const def1 = Phrase('Four', 'source')
       const def2 = Phrase('Five', 'source')
       const term = Term('a', '', def0, def1, def2)
-      console.log(term)
       expect(term.getDef(0).getName()).to.equal(def0.getName())
       expect(term.getDef(1).getName()).to.equal(def1.getName())
       expect(term.getDef(2).getName()).to.equal(def2.getName())
