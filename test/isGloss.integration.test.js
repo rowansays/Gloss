9101d588af38acda8a4b73e2910ea4400d067180
rowanweathers <74547960+rowanweathers@users.noreply.github.com>
2020-12-11 11:39:50 -0800

    isGloss(): Add integration tests.

---
+++ test/isGloss.integration.test.js
@@ -0,0 +1,14 @@
+import chai from 'chai'
+import mocha from 'mocha'
+import { isGloss } from '../src/Predicates/isGloss.js'
+import { Term } from '../src/Glosses/Term.js'
+
+var describe = mocha.describe
+var expect = chai.expect
+var it = mocha.it
+
+describe('isGloss() + various constructors.', () => {
+  it('Instances of Normal() are quotes.', function () {
+    expect(isGloss(Term('a'))).to.be.true
+  })
+})
