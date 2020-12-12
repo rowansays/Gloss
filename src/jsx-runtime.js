167bc8b9addde1e72e2c3e5de63aeaa113749ed2
rowanweathers <74547960+rowanweathers@users.noreply.github.com>
2020-12-12 14:35:13 -0800

    Directly importing @vanilla-jsx/jsx-runtime.

---
+++ src/jsx-runtime.js
@@ -0,0 +1,55 @@
+exports.jsxs = exports.jsx = (tag, { ref, children, ...props } = {}) => {
+  if (typeof tag === 'string') {
+      const element = document.createElement(tag);
+
+      Object.keys(props).forEach((key) => {
+          if (!props[key]) {
+
+          } else if (typeof props[key] === 'function') {
+              element[key] = props[key];
+          } else {
+              element.setAttribute(key, props[key]);
+          }
+      });
+
+      if (!children) {
+
+      } else if (children instanceof Array) {
+          children.forEach((child) => {
+              child && element.append(child);
+          });
+      } else {
+          element.append(children);
+      }
+
+      if (!ref) {
+
+      } else if (typeof ref === 'function') {
+          ref(element);
+      } else {
+          element.setAttribute('ref', ref)
+      }
+
+      return element;
+  } else if (typeof tag === 'function') {
+      return tag({ ref, children, ...props });
+  } else {
+      console.error('未知 tag 类型', tag);
+  }
+}
+
+exports.Fragment = ({ children } = {}) => {
+  const element = document.createDocumentFragment();
+
+  if (!children) {
+
+  } else if (children instanceof Array) {
+      children.forEach((child) => {
+          element.append(child);
+      });
+  } else {
+      element.append(children);
+  }
+
+  return element;
+}
