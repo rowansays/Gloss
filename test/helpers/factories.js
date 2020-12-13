65f7d15b17abaf1357206382bf3cd4ce8fe95d35
rowanweathers <74547960+rowanweathers@users.noreply.github.com>
2020-12-13 13:29:53 -0800

    Tests: Add helper function testNameProp().

--- test/helpers/factories.js
+++ test/helpers/factories.js
@@ -20,3 +20,32 @@ export function testFactoryFunction(name, func, instance) {
     })
   })
 }
+
+export function testNameProp (func) {
+  describe('({ name })', () => {
+    it('accepts non-empty strings.', () => {
+      expect(function () { func({ name: 'nobody' }) }).to.not.throw(Error)
+    })
+    it('accepts integers.', () => {
+      expect(function () { func({ name: 123 }) }).to.not.throw(Error)
+    })
+    it('rejects empty names', () => {
+      expect(function () { func() }).to.throw(Error)
+      expect(function () { func({ name: null }) }).to.throw(Error)
+      expect(function () { func({ name: undefined }) }).to.throw(Error)
+      expect(function () { func({ name: '' }) }).to.throw(Error)
+    })
+    it('rejects a space character.', () => {
+      expect(function () { func({ name: ' ' })}).to.throw(Error)
+    })
+    it('rejects multiple space characters.', () => {
+      expect(function () { func({ name: '   ' })}).to.throw(Error)
+    })
+    it('rejects a tab character.', () => {
+      expect(function () { func({ name: '  ' })}).to.throw(Error)
+    })
+    it('rejects multiple tab characters.', () => {
+      expect(function () { func({ name: '      ' })}).to.throw(Error)
+    })
+  })
+}
