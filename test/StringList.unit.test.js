df3511c356e5145907c5fb820360bfb120cd9e10
rowanweathers <74547960+rowanweathers@users.noreply.github.com>
2020-12-12 22:34:47 -0800

    Maybe import the correct files here?

--- test/StringList.unit.test.js
+++ test/StringList.unit.test.js
@@ -1,6 +1,6 @@
 import chai from 'chai'
 import mocha from 'mocha'
-import { StringList } from '../src/Lists/QuoteList.js'
+import { StringList } from '../src/Lists/StringList.js'
 
 var describe = mocha.describe
 var expect = chai.expect
