59bf72db1f412b76ee4f39500bb5d7b2af62cf38
rowanweathers <74547960+rowanweathers@users.noreply.github.com>
2020-12-13 14:01:59 -0800

    Add an AbstractNamed constructor.

---
+++ src/Abstracts/AbstractNamed.js
@@ -0,0 +1,26 @@
+import { castString } from '../Utility/castString.js'
+
+/**
+ * Abstract Named Object constructor.
+ *
+ * @param {object} props Required.
+ * @prop {string} name
+ * @throws TypeError
+ */
+function AbstractNamed (props) {
+  const clean = props ? castString(props.name) : ''
+  if (clean === '') {
+    throw new TypeError('' +
+      'The "props" parameter must possess a "name" property whose value is ' +
+      'either a string, a number, or an object that has a getName() method. ' +
+      `A value with type "${typeof aught}" was provided.`
+    )
+  }
+
+  this.name = clean
+}
+AbstractNamed.prototype.getName = function () {
+  return this.name
+}
+
+export { AbstractNamed }
