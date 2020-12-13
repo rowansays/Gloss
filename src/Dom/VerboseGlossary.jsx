bfaae794316c9b47e6de52cd6a4484d2303d8da2
rowanweathers <74547960+rowanweathers@users.noreply.github.com>
2020-12-12 22:57:34 -0800

    First pass at VerboseGlossary component. Needs work.

--- src/Dom/VerboseGlossary.jsx
+++ src/Dom/VerboseGlossary.jsx
@@ -1,12 +1,60 @@
-function renderGlossaryVerbose (glossary) {
-  const section = document.createElement('section')
-  const heading = document.createElement('h1')
-  const headingText = document.createTextNode(glossary.getTitle())
+import { render } from 'preact'
 
-  heading.appendChild(headingText)
-  section.classList.add('GlossaryVerbose')
-  section.appendChild(heading)
-  return section
+/**
+ * Render Verbose glossary.
+ *
+ * @return {Element} Section element.
+ */
+function renderVerboseGlossary () {
+  const fragment = document.createDocumentFragment()
+  render(VerboseGlossary(...arguments), fragment)
+  return fragment
 }
 
-export { renderGlossaryVerbose }
+function VerboseGlossary (glossary) {
+  const glosses = []
+  glossary.forEach(gloss => {
+    glosses.push(<Gloss gloss={gloss} />)
+  })
+
+  return (
+    <section class='GlossaryVerbose'>
+      <h1 class='GlossaryTitle'>{glossary.getTitle()}</h1>
+      <table class='GlossList'>
+        <thead>
+          <tr>
+            <th>Definition</th>
+            <th>References</th>
+          </tr>
+        </thead>
+        <tbody>
+          {glosses}
+        </tbody>
+      </table>
+    </section>
+  )
+}
+
+function Gloss (props) {
+  const { gloss } = props
+  const sorted = gloss.sortDefsByName()
+
+  const defs = []
+  sorted.defs.forEach((def, i) => {
+    defs.push(
+      <tr>
+        <td>{def.getName()}</td>
+        <td><q>{def.getFull()}</q></td>
+      </tr>
+    )
+  })
+
+  return (
+    <>
+      <th class='GlossName' colspan='2'>{gloss.getName()}</th>
+      {defs}
+    </>
+  )
+}
+
+export { renderVerboseGlossary, VerboseGlossary }
