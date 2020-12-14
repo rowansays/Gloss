21cd8a7e3815c48a1379a972361ad73055d43ec7
rowanweathers <74547960+rowanweathers@users.noreply.github.com>
2020-12-14 15:40:34 -0800

    Add castStringArray() utility function.

---
+++ src/Utility/castStringArray.js
@@ -0,0 +1,8 @@
+import { castString } from './castString.js'
+
+export function castStringArray(aught) {
+  aught = Array.isArray(aught) ? aught : []
+  return aught.length === 0
+    ? aught
+    : Array.from(new Set(aught)).filter(castString).filter(Boolean)
+}
