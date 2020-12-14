3769f7e34d6830bd391eb03d466fe61e8b534a3d
rowanweathers <74547960+rowanweathers@users.noreply.github.com>
2020-12-14 15:52:08 -0800

    Update isQuote() integration tests.

--- test/isQuote.integration.test.js
+++ test/isQuote.integration.test.js
@@ -1,22 +1,20 @@
 import chai from 'chai'
 import mocha from 'mocha'
 import { isQuote } from '../src/Utility/isQuote.js'
-import { Normal } from '../src/Quotes/Normal.js'
-import { Phrase } from '../src/Quotes/Phrase.js'
-import { Speech } from '../src/Quotes/Speech.js'
+import { Normal, Phrase, Quote } from '../src/Quotes/Quote.js'
 
 var describe = mocha.describe
 var expect = chai.expect
 var it = mocha.it
 
 describe('isQuote() + various constructors.', () => {
-  it('Instances of Normal() are quotes.', function () {
-    expect(isQuote(new Normal(1, 2))).to.be.true
+  it('Objects returned by Normal() are quotes.', function () {
+    expect(isQuote(Normal('a', 'b', 'c'))).to.be.true
   })
-  it('Instances of Phrase() are quotes.', function () {
-    expect(isQuote(new Phrase())).to.be.true
+  it('Objects returned by Phrase() are quotes.', function () {
+    expect(isQuote(Phrase('a', 'b'))).to.be.true
   })
-  it('Instances of Speech() are quotes.', function () {
-    expect(isQuote(new Speech())).to.be.true
+  it('Objects returned by Quote() are quotes.', function () {
+    expect(isQuote(Quote({ name: 'a', refs: ['b'] }))).to.be.true
   })
 })
