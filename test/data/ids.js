72afa072fec3c25ae52daa42536b3d865a6d6f44
rowanweathers <74547960+rowanweathers@users.noreply.github.com>
2020-12-15 13:07:56 -0800

    Tests: Add frankenstein test work data.

---
+++ test/data/ids.js
@@ -0,0 +1,9 @@
+export const frankenstein = {
+  author: 'Mary Shelley',
+  date: '1818',
+  description: 'A novel by Mary Shelly published in 1818',
+  key: 'frankenstein',
+  subtitle: 'Or, The Modern Prometheus',
+  title: 'Frankenstein',
+  url: 'https://www.gutenberg.org/ebooks/84'
+}
