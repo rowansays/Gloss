4a0e93d2e86d8b4135b25829c51ce0f37828d28d
rowansays <74547960+rowansays@users.noreply.github.com>
2020-12-16 03:15:28 -0800

    Tests: Beef up the test references.

--- test/data/ids.js
+++ test/data/ids.js
@@ -1,4 +1,4 @@
-export const frankenstein = {
+export const frankenObject = {
   author: 'Mary Shelley',
   date: '1818',
   description: 'A novel by Mary Shelly published in 1818',
@@ -6,4 +6,34 @@ export const frankenstein = {
   subtitle: 'Or, The Modern Prometheus',
   title: 'Frankenstein',
   url: 'https://www.gutenberg.org/ebooks/84'
+}
+
+export const aliceObject = {
+  author: 'Lewis Carroll',
+  date: 'November 26, 1865',
+  description: 'The Millennium Fulcrum Edition 3.0',
+  key: 'aliceInWonderland',
+  subtitle: '',
+  title: 'Alice\'s Adventures in Wonderland',
+  url: 'https://www.gutenberg.org/ebooks/11'
+}
+
+export const devilsObject = {
+  title: 'The Devil\s Dictionary',
+  subtitle: '',
+  description: '',
+  author: 'Ambrose Bierce',
+  date: '1911',
+  key: 'devilsDictionary',
+  url: 'https://www.gutenberg.org/ebooks/972'
+}
+
+export const prideObject = {
+  title: 'Pride and Prejudice',
+  subtitle: '',
+  description: '',
+  author: 'Jane Austen',
+  date: '1813',
+  key: 'prideAndPrejudice',
+  url: 'https://www.gutenberg.org/ebooks/1342'
 }
---
+++ test/data/refs.js
@@ -0,0 +1,7 @@
+import { Book } from '../../src/References/Book.js'
+import { aliceObject, devilsObject, frankenObject, prideObject } from './ids.js'
+
+export const aliceBook = Book(aliceObject)
+export const devilsBook = Book(devilsObject)
+export const frankenBook = Book(frankenObject)
+export const prideBook = Book(prideObject)
