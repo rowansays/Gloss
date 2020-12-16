58b96635d8194fb7b4150d5984bdd5ecb930099f
rowansays <74547960+rowansays@users.noreply.github.com>
2020-12-15 18:10:25 -0800

    Test: Enhance helper functions.

--- test/helpers/prototypes.js
+++ test/helpers/prototypes.js
@@ -17,13 +17,17 @@ export function test (method, instance, tests) {
     tests()
   })
 }
-
-export function testAbstractObjectListPrototype (instance) {
-  test('forEach', instance)
-  test('get', instance, () => {
-    it('returns undefined no items exist.', () => {
-      expect(instance.get(0)).to.be.undefined
-    })
+export function testMethodReturnsFrozenInstance (method, instance) {
+  it('returns an instance of the same constructor.', () => {
+    expect(instance[method]().constructor).to.equal(instance.constructor)
+  })
+  it('returns a frozen instance.', () => {
+    expect(Object.isFrozen(instance[method]())).to.be.true
+  })
+}
+export function testListInterface (instance) {
+  test('add', instance, () => {
+    testMethodReturnsFrozenInstance('add', instance)
   })
   test('entries', instance, () => {
     it('returns empty array when no items exist.', () => {
@@ -31,13 +35,33 @@ export function testAbstractObjectListPrototype (instance) {
       expect(instance.entries().length).to.equal(0)
     })
   })
-  test('getName', instance)
+  test('forEach', instance)
+  test('get', instance, () => {
+    it('returns undefined no items exist.', () => {
+      expect(instance.get(0)).to.be.undefined
+    })
+  })
   test('has', instance, () => {
     it('returns false when list is empty.', () => {
       expect(instance.has('a')).to.be.false
     })
   })
+  describe('length', () => {
+    it('is a property.', () => {
+      expect(instance).to.have.a.property('length')
+    })
+    it('is an integer.', () => {
+      expect(Number.isInteger(instance.length)).to.be.true
+    })
+    it('is zero when no items exist.', () => {
+      expect(instance.length).to.equal(0)
+    })
+  })
+}
+export function testObjectListInterface (instance) {
+  testListInterface (instance)
   test('sortAscBy', instance, () => {
+    testMethodReturnsFrozenInstance('sortDescBy', instance)
     it('returns an empty instance when no referenes exist.', () => {
       const sorted = instance.sortAscBy()
       expect(sorted.constructor).to.equal(instance.constructor)
@@ -45,6 +69,7 @@ export function testAbstractObjectListPrototype (instance) {
     })
   })
   test('sortDescBy', instance, () => {
+    testMethodReturnsFrozenInstance('sortDescBy', instance)
     it('returns an empty GlossList instance when no entries exist.', () => {
       const sorted = instance.sortDescBy()
       expect(sorted.constructor).to.equal(instance.constructor)
