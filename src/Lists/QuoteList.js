e383f0ee75e99fab92a9ead24da7fb1dc871e86a
rowansays <74547960+rowansays@users.noreply.github.com>
2020-12-17 11:44:11 -0800

    Move mergeQuotes() into QouteList().

--- src/Lists/QuoteList.js
+++ src/Lists/QuoteList.js
@@ -8,7 +8,6 @@
 
 import { AbstractObjectList } from '../Abstracts/AbstractObjectList.js'
 import { freeze } from '../Utility/freeze.js'
-import { mergeQuotes } from '../Utility/mergeQuotes.js'
 import { isQuote } from '../Utility/predicate.js'
 
 function $QuoteList (...quotes) {
@@ -27,7 +26,44 @@ $QuoteList.prototype.getItemName = function (index) {
 Object.defineProperty($QuoteList.prototype, 'constructor', {
   value: $QuoteList
 })
+/**
+ * Merge quotes.
+ *
+ * Merges all duplicate quotes into single quotes containing multiple
+ * references. To illustrate:
+ *
+ * Input array: [
+ *   Phrase { value: 'Alice', references: ['aaiw:movie'] },
+ *   Phrase { value: 'Alice', references: ['aaiw:book'] },
+ *   Phrase { value: 'Alice', references: ['aaiw:comic'] },
+ *   Phrase { value: 'Wonderland', references: ['aaiw:book'] },
+ * ]
+ *
+ * Output array: [
+ *   Phrase { value: 'Alice', references: ['aaiw:movie', 'aaiw:book', 'aaiw:comic'] },
+ *   Phrase { value: 'Wonderland', references: ['encyclopedia'] },
+ * ]
+ *
+ * @param {Quote[]} A flat array of {Quote} objects.
+ * @return {Quote{}} A flat array of {Quote} objects with duplicates merged.
+ */
+export function mergeQuotes (quotes) {
+  quotes = Array.isArray(quotes) ? quotes : []
 
+  const map = new Map()
+  quotes.forEach(quote => {
+    const key = quote.getName()
+    if (map.has(key)) {
+      // merge quotes with the same name.
+      map.set(key, map.get(key).withQuote(quote))
+    } else {
+      // Add new quote to the list.
+      map.set(key, quote)
+    }
+  })
+
+  return Array.from(map.values())
+}
 function QuoteList (...props) {
   const obj = new $QuoteList(...props)
   freeze(obj, $QuoteList)
