971386e9f74fadc4d2b694c64e0d3baf77b835ac
rowansays <74547960+rowansays@users.noreply.github.com>
2020-12-16 20:21:55 -0800

    Utility: add a couple validators.

---
+++ src/Utility/validate.js
@@ -0,0 +1,43 @@
+import { castString } from './castString.js'
+import { isReference } from './isReference.js'
+
+/**
+ * Validate reference when passed as a property.
+ *
+ * @param {string} funcName The name of the function that accepts the parameter.
+ * @param {string} funcName The name of the parameter.
+ * @param {*} aught The value to validate.
+ *
+ * @return {Reference}
+ * @throws {TypeError}
+ */
+export function validateRefProp (funcName, paramName, aught) {
+  if (!isReference(aught)) {
+    throw new TypeError('' +
+      `${funcName}() - the ${paramName} parameter must contain a single ` +
+      `reference object. A value with a type of "${typeof aught}" was ` +
+      `provided. Here is its string value: "${aught}".`
+    )
+  }
+  return aught
+}
+/**
+ * Validate non-empty string when passed as a property.
+ *
+ * @param {string} funcName The name of the function that accepts the parameter.
+ * @param {string} funcName The name of the parameter.
+ * @param {*} aught The value to validate.
+ *
+ * @return {string}
+ * @throws {TypeError}
+ */
+export function validateStringProp (funcName, paramName, aught) {
+  const clean = castString(aught)
+  if (clean === '') {
+    throw new TypeError('' +
+      `${funcName}(): the "${paramName}" parameter must be a non-empty ` +
+      `string. A value with a type of "${typeof aught}" was provided.`
+    )
+  }
+  return clean
+}
