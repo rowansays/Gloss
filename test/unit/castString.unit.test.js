fdff71575441647f7f3b3a5b1f91e96a12c0a43e
rowansays <74547960+rowansays@users.noreply.github.com>
2020-12-18 13:34:28 -0800

    Tests: simplify castString() unit tests

--- test/unit/castString.unit.test.js
+++ test/unit/castString.unit.test.js
@@ -1,39 +1,8 @@
-import { castString } from '../../src/Utility/castString.js'
+import { castString } from '../../src/Utility/cast.js'
 
 describe('castString()', () => {
-  test('is a function.', () => {
-    expect(typeof castString).toBe('function')
-  })
-  test('returns an empty string when no parameters are passed.', () => {
-    expect(castString()).toBe('')
-  })
-  test('returns an empty string for many objects.', () => {
-    expect(castString(null)).toBe('')
-    expect(castString({})).toBe('')
-    expect(castString([])).toBe('')
-  })
-  test('trims spaces from boths sides of a string.', () => {
-    expect(castString(' A ',)).toBe('A')
-  })
-  test('considers a space to be empty.', () => {
-    expect(castString(' ')).toBe('')
-    expect(castString('  ')).toBe('')
-    expect(castString('   ')).toBe('')
-  })
-  test('considers a tabs to be empty.', () => {
-    expect(castString(' ')).toBe('')
-    expect(castString('    ')).toBe('')
-    expect(castString('      ')).toBe('')
-  })
-  test('coerces numbers to strings.', () => {
-    expect(castString(123)).toBe('123')
-  })
   test('extracts the "name" value from objects with a getName() method.', () => {
-    function NamedObject (name) { this.name = name }
-    NamedObject.prototype.getName = function () { return this.name }
-    expect(castString(new NamedObject('Kublai Khan'))).toBe('Kublai Khan')
-  })
-  test('perserves valid string values.', () => {
-    expect(castString('Juniper')).toBe('Juniper')
+    const namedObject = { getName: () => 'Kublai Khan' }
+    expect(castString(namedObject)).toBe('Kublai Khan')
   })
 })
