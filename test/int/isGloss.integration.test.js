3f4f06ce06b0693fffc96f81f92e150de671999a
rowansays <74547960+rowansays@users.noreply.github.com>
2020-12-19 06:14:35 -0800

    Tests: remove individual predicate test files.

--- test/int/isGloss.integration.test.js
+++
@@ -1,8 +0,0 @@
-import { isGloss } from '../../src/Utility/predicate.js'
-import { Term } from '../../src/Glosses/Term.js'
-
-describe('isGloss() + various constructors.', () => {
-  test('Instances of Term() are glosses.', function () {
-    expect(isGloss(Term('a'))).toBe(true)
-  })
-})
