903c4190cc96ce2f03ae3dd61b9fc7867f3849d3
rowansays <74547960+rowansays@users.noreply.github.com>
2020-12-19 05:55:12 -0800

    Tests: remove individual predicate unit test files.

--- test/unit/isQuote.unit.test.js
+++
@@ -1,4 +0,0 @@
-import { isQuote } from '../../src/Utility/predicate.js'
-import { testObjectPredicate } from '../helpers/factories.js'
-
-testObjectPredicate('isQuote', isQuote)
--- test/unit/isReference.unit.test.js
+++
@@ -1,9 +0,0 @@
-import { isReference } from '../../src/Utility/predicate.js'
-import { testObjectPredicate } from '../helpers/factories.js'
-import { MockRef } from '../mocks/MockRef.js'
-
-testObjectPredicate('isReference', isReference, () => {
-  test('recognizes valid references.', async () => {
-    expect(isReference(new MockRef('abc', 'def'))).toBe(true)
-  })
-})
