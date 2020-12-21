7f47f9d90f612840040e69a5fc0569a2a94fd892
rowansays <74547960+rowansays@users.noreply.github.com>
2020-12-21 02:10:11 -0800

    Tests: add tests from $def.prototype.mapBy()

--- test/unit/Def.unit.test.js
+++ test/unit/Def.unit.test.js
@@ -3,6 +3,7 @@ import { $DefList } from '../../src/Defs/DefList.js'
 import { $RefList } from '../../src/Refs/RefList.js'
 import { $Quote } from '../../src/Quotes/Quote.js'
 import { aliceBook, devilsBook, frankenBook, prideBook } from '../data/refs.js'
+import { MockRef } from '../mocks/MockRef.js'
 
 describe('$Def', () => {
   it('is a function', () => {
@@ -62,6 +63,53 @@ describe('$Def', () => {
         new $Quote({ name: 'Goodbye', refs: prideBook }),
       )
     }).toThrow()
+  })
+  test('prototype.mapBy()', function () {
+    expect(typeof new $Def().mapBy).toBe('function')
+  })
+  it('  - returns an empty Map Instance for an empty definition', function () {
+    const d = new $Def().mapBy()
+    expect(d).toBeInstanceOf(Map)
+    expect(d.size).toBe(0)
+  })
+  it('  - maps quotes by the year their reference was published ', function () {
+    const def = new $Def(
+      new $Quote({ name: 'Hello', ref: new MockRef('The Wonderful Wizard of OZ', '1900') }),
+      new $Quote({ name: 'Hello', ref: new MockRef('Whilomville Stories', '1900') }),
+      new $Quote({ name: 'Hello', ref: new MockRef('Claudine at School', '1900') }),
+      new $Quote({ name: 'Hello', ref: new MockRef('Alice in Wonderland', '1865') }),
+      new $Quote({ name: 'Hello', ref: new MockRef('Frankenstein', '1818') }),
+    )
+    const map = def.mapBy('year')
+    expect(map).toBeInstanceOf(Map)
+    expect(map.size).toBe(3)
+
+    const _1900 = map.get('1900')
+    expect(Array.isArray(_1900)).toBe(true)
+    expect(_1900.length).toBe(3)
+
+    const _1865 = map.get('1865')
+    expect(Array.isArray(_1865)).toBe(true)
+    expect(_1865.length).toBe(1)
+
+    const _1818 = map.get('1818')
+    expect(Array.isArray(_1818)).toBe(true)
+    expect(_1818.length).toBe(1)
+  })
+  it('  - lists quotes in insertion order', function () {
+    const def = new $Def(
+      new $Quote({ name: 'Hello', ref: new MockRef('The Wonderful Wizard of OZ', '1900') }),
+      new $Quote({ name: 'Hello', ref: new MockRef('Whilomville Stories', '1900') }),
+      new $Quote({ name: 'Hello', ref: new MockRef('Claudine at School', '1900') })
+    )
+    const map = def.mapBy('year')
+    expect(map).toBeInstanceOf(Map)
+    expect(map.size).toBe(1)
+
+    const _1900 = map.get('1900')
+    expect(_1900[0].ref.name).toBe('The Wonderful Wizard of OZ')
+    expect(_1900[1].ref.name).toBe('Whilomville Stories')
+    expect(_1900[2].ref.name).toBe('Claudine at School')
   })
   test('prototype.refs()', function () {
     expect(typeof new $Def().refs).toBe('function')
