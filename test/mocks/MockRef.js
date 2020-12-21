3aff9525cebde3cd3cfbd1ba1a77b4d04cc50a5c
rowansays <74547960+rowansays@users.noreply.github.com>
2020-12-21 02:08:39 -0800

    Tests: enhance MockRef() constructor.

--- test/mocks/MockRef.js
+++ test/mocks/MockRef.js
@@ -1,11 +1,12 @@
-export function MockRef (key, name) {
+export function MockRef (name, year) {
   this.desc = ''
   this.length = 1
   this.name = name
   this.refs = []
   this.url = ''
+  this.datePublished = year
 }
 
 
-export const wikipedia = new MockRef('wikipedia', 'Wikipedia')
-export const wiktionary = new MockRef('wiktionary', 'Wiktionary')
+export const wikipedia = new MockRef('Wikipedia')
+export const wiktionary = new MockRef('Wiktionary')
